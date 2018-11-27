a4af25e91001124f8ae6fe3d20f23025e3c655e8
TalhaTZA <talhavjtza@gmail.com>
2018-11-27 21:34:18 +0500

    ADD drawer layout

--- app/src/main/java/com/example/android/navigation/MainActivity.kt
+++ app/src/main/java/com/example/android/navigation/MainActivity.kt
@@ -4,34 +4,38 @@ package com.example.android.navigation
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.NavController
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
 import androidx.navigation.ui.NavigationUI
 import com.example.android.navigation.databinding.ActivityMainBinding
+import com.example.android.navigation.databinding.FragmentTitleBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mNavController : NavController
-
+    private lateinit var mDrawerLayout: DrawerLayout
+    private lateinit var mBinding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         @Suppress("UNUSED_VARIABLE")
-        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+        mBinding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
         Init()
 
-        NavigationUI.setupActionBarWithNavController(this, mNavController)
-
+        NavigationUI.setupActionBarWithNavController(this, mNavController,mDrawerLayout)
+        NavigationUI.setupWithNavController(mBinding.navView,mNavController)
     }
 
     override fun onSupportNavigateUp(): Boolean {
         Init()
-        return mNavController.navigateUp()
+        return NavigationUI.navigateUp(mDrawerLayout,mNavController)
     }
 
     private fun Init() {
         mNavController= this.findNavController(R.id.myNavHostFragment)
+        mDrawerLayout = mBinding.drawerLayout
     }
 
 }
